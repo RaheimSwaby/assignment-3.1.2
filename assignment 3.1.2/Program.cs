49f0af42b6458e3d021719cc6393c9dd91a88acb
Rahei <Rahei@RAHEIMSPC>
2024-11-18 11:24:25 -0500

    Add project files.

---
+++ assignment 3.1.2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace assignment_3._1._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            
+            if (year % 4 == 0)
+            {
+                
+                if (year % 100 == 0)
+                {
+                    
+                    if (year % 400 == 0)
+                    {
+                        Console.WriteLine(year + " is a leap year.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(year + " is not a leap year.");
+                    }
+                }
+                else
+                {
+                    
+                    Console.WriteLine(year + " is a leap year.");
+                }
+            }
+            else
+            {
+                
+                Console.WriteLine(year + " is not a leap year.");
+            }
+        }
+    }
+}
+    
